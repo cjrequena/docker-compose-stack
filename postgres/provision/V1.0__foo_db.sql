0fb4082d5d1d7c7981cc894f2c8437740397ddff
cjrequena <cjrequena+git@protonmail.com>
2023-02-23 11:53:49 +0100

    --

---
+++ postgres/provision/V1.0__foo_db.sql
@@ -0,0 +1,60 @@
+-- Database: foo
+
+-- DROP DATABASE foo;
+
+CREATE DATABASE foo WITH  OWNER = postgres;
+
+\c foo
+
+CREATE SEQUENCE public.boo_id_seq;
+
+ALTER SEQUENCE public.boo_id_seq OWNER TO postgres;
+
+CREATE SEQUENCE public.foo_id_seq;
+
+ALTER SEQUENCE public.foo_id_seq OWNER TO postgres;
+
+-- DROP TABLE public.foo;
+
+CREATE TABLE public.foo
+(
+    id integer NOT NULL DEFAULT nextval('foo_id_seq'::regclass),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    creation_date date DEFAULT CURRENT_DATE,
+    CONSTRAINT foo_pkey PRIMARY KEY (id)
+)
+WITH (OIDS = FALSE)
+TABLESPACE pg_default;
+
+ALTER TABLE public.foo OWNER to postgres;
+
+-- Table: public.boo
+
+-- DROP TABLE public.boo;
+
+CREATE TABLE public.boo
+(
+    id integer NOT NULL DEFAULT nextval('boo_id_seq'::regclass),
+    foo_id integer,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default",
+    creation_date date DEFAULT CURRENT_DATE,
+    CONSTRAINT boo_pkey PRIMARY KEY (id),
+    CONSTRAINT boo_id_foo_id_key UNIQUE (id, foo_id),
+    CONSTRAINT fk_foo_id FOREIGN KEY (foo_id)
+    REFERENCES public.foo (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (OIDS = FALSE)
+TABLESPACE pg_default;
+
+ALTER TABLE public.boo OWNER to postgres;
+
+-- *********************************************************************************************************************
+
+\c foo
+
+INSERT INTO FOO (NAME, DESCRIPTION) VALUES ('baking soda','white powder used for cleaning and removing odours');
+INSERT INTO FOO (NAME, DESCRIPTION) VALUES ('bleach','liquid added in small amounts to water; has strong chemicals that remove stains on white clothing; also used to clean bathrooms');
+INSERT INTO FOO (NAME, DESCRIPTION) VALUES ('broom','brush with a long handle on it for sweeping floors');
+INSERT INTO FOO (NAME, DESCRIPTION) VALUES ('carpet cleaner','foam or liquid soap used on rugs and carpets');
